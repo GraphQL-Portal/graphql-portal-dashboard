45b164dd970472fec6143cf3efec43b4c0076513
Pavel Feshchenko <pavel.feschenko@gmail.com>
2020-12-23 14:34:16 +0200

    [F] basic loading screen (#52)
    Co-authored-by: F <f@Fs-MacBook-Pro.local>

--- packages/frontend/src/ui/index.ts
+++ packages/frontend/src/ui/index.ts
@@ -2,6 +2,7 @@ export { PrimaryButton, TextButton } from './Buttons';
 export { Emoji } from './Emoji';
 export { Grid, Row, Col } from './Grid';
 export { Header } from './Header';
+export { Spinner } from './Spinner';
 export { SvgIcon } from './SvgIcon';
 export { Table, TableHead, TableBody, TableRow, TableCell } from './Table';
 export { H1, H2, H3, H4, H5, H6 } from './Typography';
---
+++ packages/frontend/src/view/Loading/useStyles.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles(() => ({
+  container: {
+    display: 'flex',
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
