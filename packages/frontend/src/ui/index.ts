1f5601ca792f6c0eaa5747486da3f9b9d7f704b7
F <f@Fs-MacBook-Pro.local>
2020-12-23 18:55:45 +0200

    [F] auth flow

--- packages/frontend/src/ui/index.ts
+++ packages/frontend/src/ui/index.ts
@@ -2,11 +2,13 @@ export { PrimaryButton, TextButton } from './Buttons';
 export { Emoji } from './Emoji';
 export { Grid, Row, Col } from './Grid';
 export { Header } from './Header';
+export { Input } from './Input';
 export { Spinner } from './Spinner';
 export { SvgIcon } from './SvgIcon';
 export { Table, TableHead, TableBody, TableRow, TableCell } from './Table';
 export { H1, H2, H3, H4, H5, H6 } from './Typography';
 export {
+  Widget,
   WidgetRow,
   HugeWidget,
   BigWidget,
---
+++ packages/frontend/src/view/Login/Login.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { Content } from '../Content';
+import { LogoFull } from '../../icons';
+import { useStyles } from './useStyles';
+import { Grid, Row, Col, Widget, WidgetBody, Input, PrimaryButton, WidgetActions } from '../../ui';
+
+export const Login:React.FC = () => {
+  const { content, logo, formFrame, formField } = useStyles();
+
+  return (
+    <Content className={content}>
+      <div className={logo}>
+        <LogoFull />
+      </div>
+      <Grid>
+        <Row justify="center">
+          <Col xs={12} sm={6} md={5} lg={3}>
+            <Widget className={formFrame}>
+              <WidgetBody>
+                <Input fullWidth label="Your Email" className={formField} />
+                <Input type="password" fullWidth label="Your password" className={formField} />
+              </WidgetBody>
+              <WidgetActions>
+                <PrimaryButton fullWidth size="large">
+                  Sign In With Email
+                </PrimaryButton>
+              </WidgetActions>
+            </Widget>
+          </Col>
+        </Row>
+      </Grid>
+    </Content>
+  );
+}
