d44af5448310c5c3dc929594a7da86771b6d8681
F <f@Fs-MacBook-Pro.local>
2021-01-04 15:49:48 +0200

    [F] fix file structure

--- packages/frontend/src/model/ApiDefs/queries/deleteApiDef.ts
+++
@@ -1,6 +0,0 @@
-import { useMutation } from '@apollo/client';
-import { DELETE_API_DEF, QUERY_API_DEFS } from '../../../commands';
-
-export const useDeleteApiDef = () => {
-  return useMutation(DELETE_API_DEF, { refetchQueries: [{ query: QUERY_API_DEFS }] });
-}
--- packages/frontend/src/model/Login/commands/useLogin.ts
+++ packages/frontend/src/model/Login/commands/useLogin.ts
@@ -1,7 +1,26 @@
-import { useMutation } from '@apollo/client';
-import { LOGIN } from '../../../commands';
+import { useMutation, gql } from '@apollo/client';
 
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!, $device: String!) {
+    login(email: $email, password: $password, device: $device) {
+      accessToken
+      refreshToken
+    }
+  }
+`;
 
+// export const QUERY_USERS = gql`
+//   {
+//     getUsers {
+//       firstName
+//       lastName
+//       role
+//       email
+//       createdAt
+//       updatedAt
+//     }
+//   }
+// `;
 
 export const useLogin = (options: any) => {
   const [onLogin] = useMutation(LOGIN, options);
