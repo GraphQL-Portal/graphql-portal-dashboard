491b0c99b7851243ac82dad004c68365c07fd91d
Max <infant69ann@gmail.com>
2021-02-03 14:04:55 +0200

    unnecessary types removed

--- packages/frontend/src/view/Users/types.ts
+++
@@ -1,8 +0,0 @@
-export type UsersListFC = {
-  list: string[][];
-  data: any[];
-  onDelete: (index: number) => any;
-  onBlock: (index: number) => any;
-  onUnblock: (index: number) => any;
-  onEdit: (index: number) => any;
-}
