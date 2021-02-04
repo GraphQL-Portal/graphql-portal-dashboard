9a7fff6ee63704e30bbc6dbebd5939f43d64579b
F <f@Fs-MacBook-Pro.local>
2021-02-04 15:16:00 +0200

    [F] delete transform from the list

--- packages/frontend/src/view/DataSources/SourceTransforms/List.tsx
+++ packages/frontend/src/view/DataSources/SourceTransforms/List.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Delete } from '@material-ui/icons';
 
-import { Table, TableHead, TableBody, TableRow, TableCell } from '../../../ui';
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Tooltip,
+  IconButton,
+} from '../../../ui';
 import { getKeyFromText } from '../../../utils';
+import { TransformsList as Props } from '../../../types';
 import { TRANSFORMS_HEAD } from '../constants';
 import { useStyles } from './useStyles';
 
-type Props = {
-  transforms: any[];
-};
-
-export const List: React.FC<Props> = ({ transforms }) => {
-  const { list } = useStyles();
+export const List: React.FC<Props> = ({ transforms, onRemove }) => {
+  const { list, actionCell } = useStyles();
   if (transforms.length === 0) return null;
   return (
     <Table className={list}>
@@ -25,12 +31,24 @@ export const List: React.FC<Props> = ({ transforms }) => {
         ))}
       </TableHead>
       <TableBody>
-        {transforms.map((transform) => (
+        {transforms.map((transform, idx) => (
           <TableRow key={transform.name}>
             <TableCell>{transform.name}</TableCell>
             <TableCell>{transform.description}</TableCell>
             <TableCell />
-            <TableCell align="right" />
+            <TableCell align="right" className={actionCell}>
+              <Tooltip
+                title="Delete transform"
+                placement="left"
+                aria-label="delete transform"
+              >
+                <span>
+                  <IconButton onClick={onRemove(idx)}>
+                    <Delete />
+                  </IconButton>
+                </span>
+              </Tooltip>
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
