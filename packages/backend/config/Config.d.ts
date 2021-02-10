0ca36450b3452433baa9bcb789c5fb37f41886b4
germankrause <germanswan322@gmail.com>
2021-02-10 18:21:38 +0200

    config prettier

--- packages/backend/config/Config.d.ts
+++ packages/backend/config/Config.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-declare module "node-config-ts" {
+declare module 'node-config-ts' {
   interface IConfig {
     name: string;
     application: Application;
@@ -9,56 +9,56 @@ declare module "node-config-ts" {
     db: Db;
   }
   interface Db {
-    redis: Redis
-    mongodb: Redis
+    redis: Redis;
+    mongodb: Redis;
   }
   interface Redis {
-    connectionString: string
+    connectionString: string;
   }
   interface Client {
-    host: string
+    host: string;
   }
   interface Gateway {
     secret: string;
   }
   interface Application {
-    env: string
-    host: string
-    port: number
-    useSwaggerUi: boolean
-    graphQL: GraphQL
-    logLevel: string
-    jwtSecret: string
-    salt: string
-    defaultAdmin: DefaultAdmin
-    metrics: Metrics
-    sendgrid: Sendgrid
-    maxmind: Maxmind
+    env: string;
+    host: string;
+    port: number;
+    useSwaggerUi: boolean;
+    graphQL: GraphQL;
+    logLevel: string;
+    jwtSecret: string;
+    salt: string;
+    defaultAdmin: DefaultAdmin;
+    metrics: Metrics;
+    sendgrid: Sendgrid;
+    maxmind: Maxmind;
   }
   interface Maxmind {
-    dbPath: string
-    licenseKey: string
-    accountId: string
+    dbPath: string;
+    licenseKey: string;
+    accountId: string;
   }
   interface Sendgrid {
-    senderEmail: string
-    apiKey: string
-    confirmationTemplateId: string
-    resetPasswordTemplateId: string
+    senderEmail: string;
+    apiKey: string;
+    confirmationTemplateId: string;
+    resetPasswordTemplateId: string;
   }
   interface Metrics {
-    enabled: boolean
-    chunk: number
-    delay: number
+    enabled: boolean;
+    chunk: number;
+    delay: number;
   }
   interface DefaultAdmin {
-    email: string
-    password: string
+    email: string;
+    password: string;
   }
   interface GraphQL {
-    playground: boolean
-    debug: boolean
+    playground: boolean;
+    debug: boolean;
   }
-  export const config: Config
-  export type Config = IConfig
+  export const config: Config;
+  export type Config = IConfig;
 }
