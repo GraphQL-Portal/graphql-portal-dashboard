95e1a9a15f9fbf04749090082c18726c06971a60
F <f@Fs-MacBook-Pro.local>
2021-02-16 16:56:36 +0200

    [F] reuse suite and createAuth in useUpdateGeneral

--- packages/frontend/src/presenter/ApiDefs/useUpdateGeneral.ts
+++ packages/frontend/src/presenter/ApiDefs/useUpdateGeneral.ts
@@ -1,54 +1,13 @@
 import { useForm, useFieldArray } from 'react-hook-form';
-import vest, { test, enforce } from 'vest';
 import { vestResolver } from '@hookform/resolvers/vest';
 
 import { EditApiTab, DataSource, AError } from '../../types';
 import { useUpdateApiDef } from '../../model/ApiDefs/commands';
 import { useFormErrors } from '../../model/Hooks';
 import { useToast } from '../../model/providers';
-import {
-  arrayToFieldArray,
-  arrayStringFromObjectArray,
-} from '../DataSources/helpers';
-
-const suite = vest.create(
-  'create_new_api',
-  ({ name, endpoint, authentication }) => {
-    test('name', 'Name is required', () => {
-      enforce(name).isNotEmpty();
-    });
-
-    test('endpoint', 'Endpoint is required', () => {
-      enforce(endpoint).isNotEmpty();
-    });
-
-    test('endpoint', 'Endpoint should match pattern "^/"', () => {
-      enforce(endpoint).matches(/^\//);
-    });
-
-    const { auth_header_name, auth_tokens = [] } = authentication;
-
-    if (!!auth_header_name) {
-      test('auth_tokens', 'Auth header tokens is required', () => {
-        enforce(auth_tokens).isArray();
-      });
-
-      test('auth_tokens', 'Auth header tokens is required', () => {
-        enforce(auth_tokens[0].value).isNotEmpty();
-      });
-    }
-
-    if (auth_tokens.length > 0 && !!auth_tokens[0].value) {
-      test(
-        'authentication.auth_header_name',
-        'Auth key header is required',
-        () => {
-          enforce(auth_header_name).isNotEmpty();
-        }
-      );
-    }
-  }
-);
+import { arrayToFieldArray } from '../DataSources/helpers';
+import { createAuth } from './helpers';
+import { suite } from './validation';
 
 export const useUpdateGeneral = ({ api, refetch }: EditApiTab) => {
   const { showSuccessToast, showErrorToast } = useToast();
@@ -96,15 +55,6 @@ export const useUpdateGeneral = ({ api, refetch }: EditApiTab) => {
   } = useFieldArray({ control, name: 'authentication.auth_tokens' });
 
   const onSubmit = ({ authentication, name, endpoint }: any) => {
-    const { auth_header_name, auth_tokens } = authentication;
-    const auth = !!auth_header_name
-      ? {
-          authentication: {
-            auth_header_name,
-            auth_tokens: arrayStringFromObjectArray(auth_tokens),
-          },
-        }
-      : {};
     updateApiDef({
       variables: {
         id,
@@ -112,7 +62,7 @@ export const useUpdateGeneral = ({ api, refetch }: EditApiTab) => {
           name,
           endpoint,
           playground,
-          ...auth,
+          ...createAuth(authentication),
         },
         sources: sources.map(({ _id }: DataSource) => _id),
         enabled,
