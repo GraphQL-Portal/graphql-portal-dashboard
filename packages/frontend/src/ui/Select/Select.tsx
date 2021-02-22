d225e9bb009a17c5aed6303de1ab198062084ae1
F <f@Fs-MacBook-Pro.local>
2021-02-22 16:19:11 +0200

    [F] wrap Select with forwardRef and pass ref to root component

--- packages/frontend/src/ui/Select/Select.tsx
+++ packages/frontend/src/ui/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import {
   Select as MuiSelect,
   FormControl,
@@ -9,22 +9,20 @@ import {
 import { Select as Props } from './types';
 import { getKeyFromText } from '../../utils';
 
-export const Select: React.FC<Props> = ({
-  options,
-  fullWidth,
-  ...selectProps
-}) => {
-  const { label, labelId } = selectProps;
-  return (
-    <FormControl fullWidth={fullWidth} variant="outlined">
-      {label && <InputLabel id={labelId}>{label}</InputLabel>}
-      <MuiSelect {...selectProps}>
-        {options.map(({ value, label }) => (
-          <MenuItem value={value} key={getKeyFromText(`${value}`)}>
-            {label}
-          </MenuItem>
-        ))}
-      </MuiSelect>
-    </FormControl>
-  );
-};
+export const Select = forwardRef<unknown, Props>(
+  ({ options, fullWidth, ...selectProps }, ref) => {
+    const { label, labelId } = selectProps;
+    return (
+      <FormControl fullWidth={fullWidth} variant="outlined">
+        {label && <InputLabel id={labelId}>{label}</InputLabel>}
+        <MuiSelect {...selectProps} ref={ref}>
+          {options.map(({ value, label }) => (
+            <MenuItem value={value} key={getKeyFromText(`${value}`)}>
+              {label}
+            </MenuItem>
+          ))}
+        </MuiSelect>
+      </FormControl>
+    );
+  }
+);
