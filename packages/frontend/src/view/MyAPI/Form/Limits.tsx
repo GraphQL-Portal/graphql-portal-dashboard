fa987abd8784340bf1cfe4cde03d432e9605f8ee
F <f@Fs-MacBook-Pro.local>
2021-02-24 14:12:35 +0200

    [F] add Limits form to Create API

---
+++ packages/frontend/src/view/MyAPI/Form/Limits.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import { Col, FormGroup, Input, Row } from '../../../ui';
+import { APILimitsForm as Props } from '../../../types';
+import { useStyles } from './useStyles';
+
+export const LimitsForm: React.FC<Props> = ({ register, errors }) => {
+  const { formRow } = useStyles();
+  return (
+    <FormGroup title="Limits">
+      <Row spacing={2} className={formRow}>
+        <Col xs={6}>
+          <Input
+            name="request_size_limit"
+            label="Request size limit"
+            error={!!errors?.request_size_limit}
+            fullWidth
+            ref={register}
+          />
+        </Col>
+        <Col xs={6}>
+          <Input
+            name="request_complexity_limit"
+            label="Request complexity limit"
+            ref={register({
+              valueAsNumber: true,
+            })}
+            error={!!errors?.request_complexity_limit}
+            fullWidth
+            type="number"
+          />
+        </Col>
+      </Row>
+      <Row spacing={2} className={formRow}>
+        <Col xs={6}>
+          <Input
+            name="depth_limit"
+            label="Depth limit"
+            ref={register({
+              valueAsNumber: true,
+            })}
+            error={!!errors?.depth_limit}
+            fullWidth
+            type="number"
+          />
+        </Col>
+        <Col xs={6} />
+      </Row>
+      <Row spacing={2} className={formRow}>
+        <Col xs={6}>
+          <Input
+            name="rate_limit.complexity"
+            label="Rate limit complexity"
+            ref={register({
+              valueAsNumber: true,
+            })}
+            error={!!errors?.rate_limit?.complexity}
+            fullWidth
+            type="number"
+          />
+        </Col>
+        <Col xs={6}>
+          <Input
+            name="rate_limit.per"
+            label="Rate limit per"
+            ref={register({
+              valueAsNumber: true,
+            })}
+            error={!!errors?.rate_limit?.per}
+            fullWidth
+            type="number"
+          />
+        </Col>
+      </Row>
+    </FormGroup>
+  );
+};
--- packages/frontend/src/view/MyAPI/Form/index.ts
+++ packages/frontend/src/view/MyAPI/Form/index.ts
@@ -1,4 +1,5 @@
 export { DataSourcesForm } from './DataSources';
 export { GeneralForm } from './General';
 export { IPForm } from './IP';
+export { LimitsForm } from './Limits';
 export { SchemaForm } from './Schema';
