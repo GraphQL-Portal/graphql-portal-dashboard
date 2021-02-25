5998d9b37674a6dd7f8a85120c94ef9aa587ff2f
F <f@Fs-MacBook-Pro.local>
2021-02-25 16:34:07 +0200

    [F] refactor DataSource types

---
+++ packages/frontend/src/types/DataSource/data.ts
@@ -0,0 +1,28 @@
+import { Title, ElementsTable } from '../General';
+import { updateState } from './methods';
+
+export type SourceProp = { source: any };
+export type FormCaption = Partial<Title> & {
+  description?: string;
+};
+
+export type HandlersMapper = ElementsTable;
+
+export type TransformsMapper = ElementsTable;
+
+export type DataSourceStep<T> = {
+  updateState: updateState<T>;
+  state: T;
+  step: number;
+};
+
+export type NameForm = { name: string };
+export type HandlerForm = { handler: any };
+export type TransformsForm = { transforms: any[] };
+
+export type DataSource = NameForm &
+  HandlerForm &
+  TransformsForm & {
+    _id?: string;
+    updatedAt?: string;
+  };
---
+++ packages/frontend/src/types/DataSource/index.ts
@@ -0,0 +1,4 @@
+export * from './components';
+export * from './data';
+export * from './forms';
+export * from './methods';
