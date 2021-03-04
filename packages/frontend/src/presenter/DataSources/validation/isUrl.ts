c9bef27934ef9c801d973cb085449632adad6683
Pavel Feshchenko <pavel.feschenko@gmail.com>
2021-03-04 22:02:18 +0200

    [F] fix validation RegExp (#309)
    Co-authored-by: F <f@Fs-MacBook-Pro.local>

--- packages/frontend/src/presenter/DataSources/validation/isUrl.ts
+++ packages/frontend/src/presenter/DataSources/validation/isUrl.ts
@@ -1,5 +1,5 @@
 import { enforce } from 'vest';
 
-const URL_EXP = /^http[s]?:\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*(\??(&?\w*=\w*)+)?$/;
+const URL_EXP = /^http[s]?:\/\/\w+(\.\w+)*(:[0-9]+)*(\/\w+)*\??(&?\w+=[-\w]+)*$/;
 
 export const isUrl = (url: string) => enforce(url).matches(URL_EXP);
