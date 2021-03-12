059eebe629abebc02a400f7f0e0cffaa699a797e
Pavel Feshchenko <pavel.feschenko@gmail.com>
2021-03-12 13:21:02 +0200

    [F] hide group name if items is empty (#341)
    Co-authored-by: F <f@Fs-MacBook-Pro.local>

--- packages/frontend/src/view/Navigation/Main/Group.tsx
+++ packages/frontend/src/view/Navigation/Main/Group.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 import { getKeyFromText } from '../../../utils/getKeyFromText';
 import { Group as Props } from '../../../types';
 import { H6 } from '../../../ui';
+import { isZeroLength } from '../../../utils';
 import { RoleProtectedComponent } from '../../RoleProtectedComponent';
 import { Item } from './Item';
 import { useStyles } from './useStyles';
 
 export const Group: React.FC<Props> = ({ name, items }) => {
   const { group, groupName } = useStyles();
+  if (isZeroLength(items)) return null;
+
   return (
     <section className={group}>
       {!!name && <H6 className={groupName}>{name}</H6>}
-      {items.map((item) => {
+      {items.map(item => {
         return (
           <RoleProtectedComponent
             Component={Item}
