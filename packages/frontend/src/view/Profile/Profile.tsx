31fdfc84f05df5e2efae58a97aeb94c0fa9bb997
F <f@Fs-MacBook-Pro.local>
2021-03-23 10:42:28 +0200

    [F] update layout

--- packages/frontend/src/view/Profile/Profile.tsx
+++ packages/frontend/src/view/Profile/Profile.tsx
@@ -8,6 +8,8 @@ import {
   Input,
   PrimaryButton,
   Row,
+  WidgetBody,
+  WidgetHeader,
   WidgetRow,
 } from '../../ui';
 import { useProfile } from '../../presenter/Users';
@@ -25,48 +27,51 @@ export const Profile: React.FC = () => {
       <Helmet>
         <title>User profile page</title>
       </Helmet>
-      <Header title="Edit Your Profile">
+      <Header title="Your Profile">
         <PrimaryButton onClick={signOut}>Sign Out</PrimaryButton>
       </Header>
       <WidgetRow>
         <HugeWidget>
-          <form onSubmit={onSubmit}>
-            <Row spacing={2} className={formRow}>
-              <Col xs={6}>
-                <Input
-                  name="firstName"
-                  ref={register}
-                  error={!!errors?.firstName}
-                  fullWidth
-                  label="First name"
-                />
-              </Col>
-            </Row>
-            <Row spacing={2} className={formRow}>
-              <Col xs={6}>
-                <Input
-                  name="lastName"
-                  ref={register}
-                  error={!!errors?.lastName}
-                  fullWidth
-                  label="Last name"
-                />
-              </Col>
-            </Row>
-            <Row spacing={2} className={formRow}>
-              <Col xs={6}>
-                <Input
-                  name="email"
-                  ref={register}
-                  error={!!errors?.email}
-                  fullWidth
-                  label="Email"
-                  disabled
-                />
-              </Col>
-            </Row>
-            <PrimaryButton type="submit">Update Profile</PrimaryButton>
-          </form>
+          <WidgetHeader title="Account information" />
+          <WidgetBody>
+            <form onSubmit={onSubmit}>
+              <Row spacing={2} className={formRow}>
+                <Col xs={6}>
+                  <Input
+                    name="firstName"
+                    ref={register}
+                    error={!!errors?.firstName}
+                    fullWidth
+                    label="First name"
+                  />
+                </Col>
+              </Row>
+              <Row spacing={2} className={formRow}>
+                <Col xs={6}>
+                  <Input
+                    name="lastName"
+                    ref={register}
+                    error={!!errors?.lastName}
+                    fullWidth
+                    label="Last name"
+                  />
+                </Col>
+              </Row>
+              <Row spacing={2} className={formRow}>
+                <Col xs={6}>
+                  <Input
+                    name="email"
+                    ref={register}
+                    error={!!errors?.email}
+                    fullWidth
+                    label="Email"
+                    disabled
+                  />
+                </Col>
+              </Row>
+              <PrimaryButton type="submit">Update Profile</PrimaryButton>
+            </form>
+          </WidgetBody>
         </HugeWidget>
       </WidgetRow>
     </>
