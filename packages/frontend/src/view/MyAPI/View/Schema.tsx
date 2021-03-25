094c0477819541a66c3c201d0c07f98d6c1cd302
F <f@Fs-MacBook-Pro.local>
2021-03-25 10:09:15 +0200

    [F] word wrap for codemirror

--- packages/frontend/src/view/MyAPI/View/Schema.tsx
+++ packages/frontend/src/view/MyAPI/View/Schema.tsx
@@ -2,21 +2,24 @@ import React from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import 'codemirror-graphql/mode';
 import 'codemirror/theme/dracula.css';
+import clsx from 'clsx';
 
 import { ViewAPITab as Props } from '../../../types';
 import { useStyles } from './useStyles';
 import { useAPIViewSchema } from '../../../presenter/ApiDefs';
 
 export const Schema: React.FC<Props> = ({ fetcher }) => {
-  const { wrapper } = useStyles();
+  const { wrapper, editor } = useStyles();
   const { value } = useAPIViewSchema(fetcher);
+  const editorClassName = clsx(wrapper, editor);
 
   return (
-    <div className={wrapper}>
+    <div className={editorClassName}>
       <CodeMirror
         value={value}
         options={{
           mode: 'graphql',
+          lineWrapping: true,
           tabSize: 2,
           readOnly: 'nocursor',
           theme: 'dracula',
