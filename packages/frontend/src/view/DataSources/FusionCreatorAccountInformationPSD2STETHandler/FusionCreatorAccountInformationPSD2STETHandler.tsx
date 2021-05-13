657c4de84c62af33f92dd94df581c835909517db
F <f@Fs-MacBook-Pro.local>
2021-05-13 13:48:27 +0300

    [F] update FusionCreator PSD2STET

--- packages/frontend/src/view/DataSources/FusionCreatorAccountInformationPSD2STETHandler/FusionCreatorAccountInformationPSD2STETHandler.tsx
+++ packages/frontend/src/view/DataSources/FusionCreatorAccountInformationPSD2STETHandler/FusionCreatorAccountInformationPSD2STETHandler.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
-import { Controller } from 'react-hook-form';
 import { Input, PrimaryButton } from '../../../ui';
 import { useFusionCreatorAccountInformationPSD2STETHandler } from '../../../presenter/DataSources';
 import { HandlerStep } from '../../../types';
 import { HandlerCol, HandlerRow } from '../Layout';
-import { getError } from '../helpers';
 
 export const FusionCreatorAccountInformationPSD2STETHandler: React.FC<HandlerStep> = (
   props
 ) => {
   const {
-    control,
+    register,
     errors,
     onSubmit,
   } = useFusionCreatorAccountInformationPSD2STETHandler(props);
-  const hasErrors = getError(errors);
 
   return (
     <form noValidate autoComplete="off" onSubmit={onSubmit}>
       <HandlerRow>
         <HandlerCol>
-          <Controller
-            as={Input}
-            control={control}
+          <Input
+            ref={register}
             label="Authorization token"
             required
             name="token"
-            error={hasErrors('token')}
+            error={!!errors?.token}
             fullWidth
           />
         </HandlerCol>
