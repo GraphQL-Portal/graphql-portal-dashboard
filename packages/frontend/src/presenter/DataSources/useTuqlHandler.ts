44223874b36faa821689bc5265806f8041749e27
F <f@Fs-MacBook-Pro.local>
2021-05-13 17:50:41 +0300

    [F] update Tuql Handler form

--- packages/frontend/src/presenter/DataSources/useTuqlHandler.ts
+++ packages/frontend/src/presenter/DataSources/useTuqlHandler.ts
@@ -1,25 +1,29 @@
 import { useForm } from 'react-hook-form';
 import { useFormErrors } from '../../model/Hooks';
-import { HandlerStep } from '../../types';
+import { TuqlForm, UseTuqlHandlerHook } from '../../types';
 
 const TUQL_DEFAULT_STATE = {
-  db: '',
-  infile: '',
+  db: undefined,
+  infile: undefined,
 };
 
-export const useTuqlHandler = ({ state, updateState, step }: HandlerStep) => {
+export const useTuqlHandler: UseTuqlHandlerHook = ({
+  state,
+  updateState,
+  step,
+}) => {
   const defaultValues = Object.assign({}, TUQL_DEFAULT_STATE, state.handler);
-  const { handleSubmit, errors, control } = useForm({
+  const { handleSubmit, errors, register } = useForm<TuqlForm>({
     defaultValues,
   });
 
   useFormErrors(errors);
 
-  const onSubmit = (handler: any) => updateState({ handler }, step);
+  const onSubmit = (handler: TuqlForm) => updateState({ handler }, step);
 
   return {
     onSubmit: handleSubmit(onSubmit),
     errors,
-    control,
+    register,
   };
 };
