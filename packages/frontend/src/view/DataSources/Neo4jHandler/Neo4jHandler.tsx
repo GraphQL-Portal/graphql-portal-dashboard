2a1a6aac5adec33405e573d31a36cb455643a9ce
F <f@Fs-MacBook-Pro.local>
2021-05-13 16:41:04 +0300

    [F] update Neo4J handler

--- packages/frontend/src/view/DataSources/Neo4jHandler/Neo4jHandler.tsx
+++ packages/frontend/src/view/DataSources/Neo4jHandler/Neo4jHandler.tsx
@@ -5,36 +5,32 @@ import { Input, Checkbox, PrimaryButton, FormLabel } from '../../../ui';
 import { useNeo4jHandler } from '../../../presenter/DataSources';
 import { HandlerStep } from '../../../types';
 import { HandlerCol, HandlerRow } from '../Layout';
-import { getError } from '../helpers';
 
 export const Neo4jHandler: React.FC<HandlerStep> = (props) => {
-  const { control, errors, onSubmit } = useNeo4jHandler(props);
-  const hasErrors = getError(errors);
+  const { control, errors, onSubmit, register } = useNeo4jHandler(props);
 
   return (
     <form noValidate autoComplete="off" onSubmit={onSubmit}>
       <HandlerRow>
         <HandlerCol>
-          <Controller
-            as={Input}
-            control={control}
+          <Input
+            ref={register}
             label="Url"
             required
             name="url"
-            error={hasErrors('url')}
+            error={!!errors?.url}
             fullWidth
           />
         </HandlerCol>
       </HandlerRow>
       <HandlerRow>
         <HandlerCol>
-          <Controller
-            as={Input}
-            control={control}
+          <Input
+            ref={register}
             label="Username"
             required
             name="username"
-            error={hasErrors('username')}
+            error={!!errors?.username}
             fullWidth
             InputProps={{
               autoComplete: 'off',
@@ -44,14 +40,13 @@ export const Neo4jHandler: React.FC<HandlerStep> = (props) => {
       </HandlerRow>
       <HandlerRow>
         <HandlerCol>
-          <Controller
-            as={Input}
-            control={control}
+          <Input
+            ref={register}
             label="Password"
             required
             name="password"
             type="password"
-            error={hasErrors('password')}
+            error={!!errors?.password}
             fullWidth
             InputProps={{
               autoComplete: 'off',
@@ -61,24 +56,22 @@ export const Neo4jHandler: React.FC<HandlerStep> = (props) => {
       </HandlerRow>
       <HandlerRow>
         <HandlerCol>
-          <Controller
-            as={Input}
-            control={control}
+          <Input
+            ref={register}
             label="Database"
             name="database"
-            error={hasErrors('database')}
+            error={!!errors?.database}
             fullWidth
           />
         </HandlerCol>
       </HandlerRow>
       <HandlerRow>
         <HandlerCol>
-          <Controller
-            as={Input}
-            control={control}
+          <Input
+            ref={register}
             label="Type definitions"
             name="typeDefs"
-            error={hasErrors('typeDefs')}
+            error={!!errors?.typeDefs}
             fullWidth
           />
         </HandlerCol>
