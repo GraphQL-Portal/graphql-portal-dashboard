30180c7c8527e045a90335e5ae1e00a69f1ddfab
Max <infant69ann@gmail.com>
2021-06-10 17:10:51 +0300

    autoLogin effect

--- packages/frontend/src/model/providers/State/config.ts
+++ packages/frontend/src/model/providers/State/config.ts
@@ -1,5 +1,14 @@
-const { REACT_APP_DASHBOARD_URL, REACT_APP_DASHBOARD_WS_URL } = process.env;
+const {
+  REACT_APP_DASHBOARD_URL,
+  REACT_APP_DASHBOARD_WS_URL,
+  AUTO_LOGIN_ENABLED,
+  DEFAULT_ADMIN_EMAIL,
+  DEFAULT_ADMIN_PASSWORD,
+} = process.env;
 
 export const URI = REACT_APP_DASHBOARD_URL || 'http://localhost:3030/graphql';
 export const wsURI =
   REACT_APP_DASHBOARD_WS_URL || 'ws://localhost:3030/graphql';
+export const autoLoginEnabled = AUTO_LOGIN_ENABLED;
+export const defaultAdminEmail = DEFAULT_ADMIN_EMAIL || 'admin@example.com';
+export const defaultAdminPassword = DEFAULT_ADMIN_PASSWORD || 'Secret123!';
